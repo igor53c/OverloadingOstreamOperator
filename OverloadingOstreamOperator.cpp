4c878df47f2e153eaa61e6d2a0c2607f15025b51
Igor <nesto@nesto.co>
2022-05-12 22:08:00 +0200

    Add project files.

---
+++ OverloadingOstreamOperator.cpp
@@ -0,0 +1,36 @@
+// OverloadingOstreamOperator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Person {
+public:
+	Person(const string& first_name, const string& last_name) : first_name_(first_name), last_name_(last_name) {}
+	const string& get_first_name() const {
+		return first_name_;
+	}
+	const string& get_last_name() const {
+		return last_name_;
+	}
+private:
+	string first_name_;
+	string last_name_;
+};
+
+ostream& operator<<(ostream& os, Person& p)
+{
+	os << "first_name=" << p.get_first_name() << ",last_name=" << p.get_last_name();
+
+	return os;
+}
+
+
+int main() {
+	string first_name, last_name, event;
+	cin >> first_name >> last_name >> event;
+	auto p = Person(first_name, last_name);
+	cout << p << " " << event << endl;
+	return 0;
+}
